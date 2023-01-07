6df938d4be8ff2752034344c46b0f06638ab626e
Pc <belhadjisoumaya@gmail.com>
2023-01-07 18:15:19 +0100

    first commit

--- DeepLearning_Project-Frontend/src/app/app-routing.module.ts
+++ DeepLearning_Project-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PredictComponent } from './components/predict/predict.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PredictComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DeepLearning_Project-Frontend/src/app/app.module.ts
+++ DeepLearning_Project-Frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { PredictComponent } from './components/predict/predict.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PredictComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
